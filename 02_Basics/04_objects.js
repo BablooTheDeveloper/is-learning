4a40b9abd336b121d737fa3d58619205b0ebdf23
BablooTheDeveloper <professional.mi12@gmail.com>
2024-08-15 16:39:25 +0000

    04_objects

---
+++ 02_Basics/04_objects.js
@@ -0,0 +1,91 @@
+// const tinderUser = new Object()
+// Above object is declared using constructor
+
+const tinderUser = {}
+
+tinderUser.id = "123abc"
+tinderUser.name = "Sammy"
+tinderUser.isLoggedIn = false
+
+// console.log(tinderUser);
+
+const regularUser = {
+    email: "some@gmail.com",
+    fullname: {
+        userfullname: {
+            firstname: "hitesh",
+            lastname: "choudhary"
+        }
+    }
+}
+
+// console.log(regularUser.fullname.userfullname.firstname);
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+const obj4 = {5: "a", 6: "b"}
+
+// const obj3 = { obj1, obj2 }
+// const obj3 = Object.assign({}, obj1, obj2, obj4)
+// "assign()" is used for combining the objects.
+// It is good practice to first give {} in assign() as a target and then other objects
+
+const obj3 = {...obj1, ...obj2}
+// console.log(obj3);
+// But here also we can use "spreading" to combine objects
+
+
+const users = [
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+]
+
+users[1].email
+// console.log(tinderUser);
+
+// console.log(Object.keys(tinderUser));
+// Gives array of keys from the object
+
+// console.log(Object.values(tinderUser));
+// Gives array of values from the object
+
+// console.log(Object.entries(tinderUser));
+// Gives array of each entries from the object
+
+// console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+// checks whether the given property is there in an object or not.
+
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "hitesh"
+}
+
+// course.courseInstructor
+
+const {courseInstructor: instructor} = course
+
+// console.log(courseInstructor);
+console.log(instructor);
+
+// {
+//     "name": "hitesh",
+//     "coursename": "js in hindi",
+//     "price": "free"
+// }
+
+[
+    {},
+    {},
+    {}
+]
